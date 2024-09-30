9534d1bc428b1aadcd6befd8fa1030d7bb7278dc
User <user@192.168.1.5>
2024-09-30 17:50:27 +0200

    Initial commit of Telegram bot boilerplate

---
+++ bot/main.py
@@ -0,0 +1,24 @@
+import logging
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from config import TOKEN
+from handlers.message_handlers import start, echo
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+def main() -> None:
+    # Create the Application and pass it your bot's token
+    application = Application.builder().token(TOKEN).build()
+
+    # Add handlers
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
